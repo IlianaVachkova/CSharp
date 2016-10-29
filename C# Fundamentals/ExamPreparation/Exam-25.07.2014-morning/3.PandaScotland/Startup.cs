da9bf8da48c81d996986b55c5f883fa4614ef03b
IlianaVachkova <an_vachkova@yahoo.com>
2016-10-30 01:17:53 +0300

    Added tasks solution for Exam-25.07.2014-morning.

---
+++ C# Fundamentals/ExamPreparation/Exam-25.07.2014-morning/3.PandaScotland/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PandaScotland
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int letterLeft = 65;
+            int letterRight = 66;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (letterLeft == 90)
+                {
+                    letterRight = 65;
+                }
+
+                Console.WriteLine(new string('~', i) + (char)letterLeft + new string('#', n - 2 - 2 * i) + (char)letterRight + new string('~', i));
+
+                if (letterRight == 90)
+                {
+                    letterLeft = 65;
+                    letterRight = 66;
+                }
+                else if (letterRight == 65)
+                {
+                    letterLeft = 66;
+                    letterRight = 67;
+                }
+                else
+                {
+                    letterLeft += 2;
+                    letterRight += 2;
+                }
+            }
+
+            Console.WriteLine(new string('-', n / 2) + (char)letterLeft + new string('-', n / 2));
+
+            letterRight = letterLeft + 1;
+            letterLeft = letterRight + 1;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                if (letterRight == 90)
+                {
+                    letterLeft = 65;
+                }
+
+                Console.WriteLine(new string('~', i) + (char)letterRight + new string('#', n - 2 - 2 * i) + (char)letterLeft + new string('~', i));
+
+                if (letterLeft == 90)
+                {
+                    letterRight = 65;
+                }
+                else if (letterLeft == 65)
+                {
+                    letterRight = 66;
+                    letterLeft = 67;
+                }
+                else
+                {
+                    letterRight += 2;
+                    letterLeft += 2;
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/ExamPreparation/Exam-25.07.2014-morning/4.MorseCodeNumbers/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _4.MorseCodeNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string number = Console.ReadLine();
+            int nSum = 0;
+
+            string[] morseCodeNumbers = { "-----|", ".----|", "..---|", "...--|", "....-|", ".....|" };
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                nSum += (number[i] - '0');
+            }
+
+            int count = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    for (int l = 0; l <= 5; l++)
+                    {
+                        for (int k = 0; k <= 5; k++)
+                        {
+                            for (int m = 0; m <= 5; m++)
+                            {
+                                for (int p = 0; p <= 5; p++)
+                                {
+                                    if (i * j * l * k * m * p == nSum)
+                                    {
+                                        Console.WriteLine(morseCodeNumbers[i] + morseCodeNumbers[j] + morseCodeNumbers[l]
+                                                    + morseCodeNumbers[k] + morseCodeNumbers[m] + morseCodeNumbers[p]);
+
+                                        count += 1;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
