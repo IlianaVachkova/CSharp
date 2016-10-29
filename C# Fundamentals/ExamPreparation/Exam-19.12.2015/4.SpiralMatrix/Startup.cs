f10966d2abe8d9ed6967d387dabf640696a396fe
IlianaVachkova <an_vachkova@yahoo.com>
2016-10-30 00:43:22 +0300

    Added tasks solution for Exam-19.12.2015.

---
+++ C# Fundamentals/ExamPreparation/Exam-19.12.2015/4.SpiralMatrix/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace SpiralMatrix
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nPrint = n;
+            int[,] matrix = new int[n, n];
+
+            string input = Console.ReadLine();
+            string inputLowerCase = input.ToLower();
+
+            int row = 0;
+            int col = 0;
+            int strPositionCount = -1;
+            for (int i = 0; i < n; i++)
+            {
+                strPositionCount++;
+
+                if (i % inputLowerCase.Length == 0)
+                {
+                    strPositionCount = 0;
+                }
+                matrix[row, i] = (inputLowerCase[strPositionCount] - 96) * 10;
+
+            }
+            col = n - 1;
+            int direction = 0;
+
+            while (n > 0)
+            {
+                for (int repeats = 0; repeats < 2; repeats++)
+                {
+                    for (int depth = 0; depth < n - 1; depth++)
+                    {
+                        switch (direction)
+                        {
+                            case 0:
+                                row++;
+                                break;
+                            case 1:
+                                col--;
+                                break;
+                            case 2:
+                                row--;
+                                break;
+                            case 3:
+                                col++;
+                                break;
+                            default:
+                                break;
+                        }
+                        strPositionCount++;
+                        if (strPositionCount == inputLowerCase.Length)
+                        {
+                            strPositionCount = 0;
+                        }
+                        matrix[row, col] = (inputLowerCase[strPositionCount] - 96) * 10;
+                    }
+                    direction++;
+                    direction %= 4;
+                }
+                n--;
+            }
+
+            int sum = 0;
+            int maxSum = 0;
+            int rowWithMaxSum = 0;
+            for (int i = 0; i < nPrint; i++)
+            {
+                for (int j = 0; j < nPrint; j++)
+                {
+                    sum += matrix[i, j];
+                }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    rowWithMaxSum = i;
+                }
+                sum = 0;
+            }
+
+            Console.WriteLine("{0} - {1}", rowWithMaxSum, maxSum);
+        }
+    }
+}
---
+++ C# Fundamentals/ExamPreparation/Exam-19.12.2015/5.PaintBall/Startup.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace PaintBall
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int[,] board = new int[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    board[i, j] = 1;
+                }
+            }
+
+            bool blackPaintShot = true;
+            while (true)
+            {
+                string shot = Console.ReadLine();
+
+                if (shot == "End")
+                {
+                    break;
+                }
+
+                string[] shotAsArr = shot.Split(' ');
+                int row = int.Parse(shotAsArr[0]);
+                int col = 9 - int.Parse(shotAsArr[1]);
+                int radius = int.Parse(shotAsArr[2]);
+
+                int startRow, endRow, startCol, endCol;
+
+                if (row - radius < 0)
+                {
+                    startRow = 0;
+                }
+                else
+                {
+                    startRow = row - radius;
+                }
+
+                if (row + radius > 9)
+                {
+                    endRow = 9;
+                }
+                else
+                {
+                    endRow = row + radius;
+                }
+
+                if (col - radius < 0)
+                {
+                    startCol = 0;
+                }
+                else
+                {
+                    startCol = col - radius;
+                }
+
+                if (col + radius > 9)
+                {
+                    endCol = 9;
+                }
+                else
+                {
+                    endCol = col + radius;
+                }
+
+                for (int rowShot = startRow; rowShot <= endRow; rowShot++)
+                {
+                    for (int colShot = startCol; colShot <= endCol; colShot++)
+                    {
+                        if (blackPaintShot)
+                        {
+                            board[rowShot, colShot] = 0;
+                        }
+                        else if (!blackPaintShot)
+                        {
+                            board[rowShot, colShot] = 1;
+                        }
+                    }
+                }
+
+                if (blackPaintShot)
+                {
+                    blackPaintShot = false;
+                }
+                else
+                {
+                    blackPaintShot = true;
+                }
+            }
+
+            string resultAsString = string.Empty;
+            int sum = 0;
+            int resultAsNumber = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    resultAsString += board[i, j];
+                }
+
+                resultAsNumber = Convert.ToInt32(resultAsString, 2);
+                sum += resultAsNumber;
+                resultAsString = string.Empty;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
