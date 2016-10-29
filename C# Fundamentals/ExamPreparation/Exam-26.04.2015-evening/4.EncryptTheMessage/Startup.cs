14f4415de460010636f963dec4b67da32d6287d8
IlianaVachkova <an_vachkova@yahoo.com>
2016-10-30 01:29:17 +0300

    Added tasks solution for Exam-26.04.2015-evening.

---
+++ C# Fundamentals/ExamPreparation/Exam-26.04.2015-evening/4.EncryptTheMessage/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+
+namespace _4.EncryptTheMessage
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int countMessages = 0;
+            StringBuilder allMessagesInOne = new StringBuilder();
+
+            bool canStartCounting = false;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "START" || command == "start")
+                {
+                    canStartCounting = true;
+                    break;
+                }
+            }
+            
+            while (canStartCounting)
+            {
+                string command = Console.ReadLine();
+                string result = string.Empty;
+
+                if (command == "END" || command == "end")
+                {
+                    if (countMessages > 0)
+                    {
+                        Console.WriteLine("Total number of messages: {0}", countMessages);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No messages sent.");
+                    }
+
+                    break;
+                }
+
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    if ((command[i] >= 65 && command[i] <= 77) || (command[i] >= 97 && command[i] <= 109))
+                    {
+                        result += (char)(command[i] + 13);
+                    }
+                    else if ((command[i] >= 78 && command[i] <= 90) || (command[i] >= 110 && command[i] <= 122))
+                    {
+                        result += (char)(command[i] - 13);
+                    }
+                    else if (command[i] >= 48 && command[i] <= 57)
+                    {
+                        result += (char)command[i];
+                    }
+                    else
+                    {
+                        switch (command[i])
+                        {
+                            case ' ': result += '+'; break;
+                            case '+': result += ' '; break;
+                            case ',': result += '%'; break;
+                            case '%': result += ','; break;
+                            case '.': result += '&'; break;
+                            case '&': result += '.'; break;
+                            case '?': result += '#'; break;
+                            case '#': result += '?'; break;
+                            case '!': result += '$'; break;
+                            case '$': result += '!'; break;
+                        }
+                    }
+                }
+
+                if (result != "")
+                {
+                    countMessages += 1;
+                    allMessagesInOne.AppendLine(result);
+                }
+            }
+
+            Console.WriteLine(allMessagesInOne);
+        }
+    }
+}
---
+++ C# Fundamentals/ExamPreparation/Exam-26.04.2015-evening/5.GameOfLife/Startup.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace _5.GameOfLife_2
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int[,] matrixInput = new int[10, 10];
+            int[,] matrixResult = new int[10, 10];
+
+            int numberOfCoordinates = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCoordinates; i++)
+            {
+                int row = int.Parse(Console.ReadLine());
+                int col = int.Parse(Console.ReadLine());
+
+                matrixInput[row, 9 - col] = 1;
+            }
+
+            int startRow, endRow, startCol, endCol;
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    if (row == 0)
+                    {
+                        startRow = 0;
+                    }
+                    else
+                    {
+                        startRow = row - 1;
+                    }
+
+                    if (row == 9)
+                    {
+                        endRow = 9;
+                    }
+                    else
+                    {
+                        endRow = row + 1;
+                    }
+
+                    if (col == 0)
+                    {
+                        startCol = 0;
+                    }
+                    else
+                    {
+                        startCol = col - 1;
+                    }
+
+                    if (col == 9)
+                    {
+                        endCol = 9;
+                    }
+                    else
+                    {
+                        endCol = col + 1;
+                    }
+
+                    int countZeros = 0;
+                    int countOnes = 0;
+                    for (int i = startRow; i <= endRow; i++)
+                    {
+                        for (int j = startCol; j <= endCol; j++)
+                        {
+                            if (matrixInput[i, j] == 0)
+                            {
+                                countZeros += 1;
+                            }
+                            else if (matrixInput[i, j] == 1)
+                            {
+                                countOnes += 1;
+                            }
+                        }
+                    }
+
+                    if (matrixInput[row, col] == 1)
+                    {
+                        countOnes -= 1;
+                    }
+                    else if (matrixInput[row, col] == 0)
+                    {
+                        countZeros -= 1;
+                    }
+
+                    if (matrixInput[row, col] == 0 && countOnes == 3)
+                    {
+                        matrixResult[row, col] = 1;
+                    }
+                    else if (matrixInput[row, col] == 1)
+                    {
+                        if (countOnes == 1)
+                        {
+                            matrixResult[row, col] = 0;
+                        }
+                        else if (countOnes == 2 || countOnes == 3)
+                        {
+                            matrixResult[row, col] = 1;
+                        }
+                        else if (countOnes >= 4)
+                        {
+                            matrixResult[row, col] = 0;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(matrixResult[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
