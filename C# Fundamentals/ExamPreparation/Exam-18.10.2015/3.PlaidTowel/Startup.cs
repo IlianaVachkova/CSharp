6eac74ab0e9f0d61070fe24810665e1a824e0129
IlianaVachkova <an_vachkova@yahoo.com>
2016-10-29 17:46:57 +0300

    Added tasks solution for Exam-18.10.2015.

---
+++ C# Fundamentals/ExamPreparation/Exam-18.10.2015/3.PlaidTowel/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _3.PlaidTowel
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char backgroundSymbol = char.Parse(Console.ReadLine());
+            char rhombusSymbol = char.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size * 4 + 1, size * 4 + 1];
+
+            for (int i = 0; i < size * 4 + 1; i++)
+            {
+                for (int j = 0; j < size * 4 + 1; j++)
+                {
+                    if ((i + j) == size || (i + j) == size * 3 || (i + j) == size * 5 || (i + j) == size * 7 || (i + j) == (size * 3 + 2 * i)
+                        || (i + j) == (size + 2 * j) || (i + j) == (size + 2 * i) || (i + j) == (size * 3 + 2 * j))
+                    {
+                        matrix[i, j] = rhombusSymbol;
+                    }
+                    else
+                    {
+                        matrix[i, j] = backgroundSymbol;
+                    }
+                }
+            }
+
+            for (int i = 0; i < size * 4 + 1; i++)
+            {
+                for (int j = 0; j < size * 4 + 1; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/ExamPreparation/Exam-18.10.2015/4.Firefighters/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace FirefightingOrganization
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int firefighters = int.Parse(Console.ReadLine());
+            int totalKids = 0;
+            int totalAdults = 0;
+            int totalSeniors = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                int kidsCount = 0;
+                int adultsCount = 0;
+                int seniorCount = 0;
+                int restForAdults = 0;
+                int restForSeniors = 0;
+
+                if (line == "rain")
+                {
+                    break;
+                }
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'K': kidsCount += 1; break;
+                        case 'A': adultsCount += 1; break;
+                        case 'S': seniorCount += 1; break;
+                    }
+                }
+
+                if (kidsCount >= firefighters)
+                {
+                    totalKids += firefighters;
+                }
+                else if (kidsCount < firefighters)
+                {
+                    restForAdults = firefighters - kidsCount;
+                    totalKids += kidsCount;
+                }
+
+                if (adultsCount >= restForAdults)
+                {
+                    totalAdults += restForAdults;
+                }
+                else if (adultsCount < restForAdults)
+                {
+                    restForSeniors = restForAdults - adultsCount;
+                    totalAdults += adultsCount;
+                }
+
+                if (seniorCount >= restForSeniors)
+                {
+                    totalSeniors += restForSeniors;
+                }
+                else
+                {
+                    totalSeniors += seniorCount;
+                }
+
+            }
+
+            Console.WriteLine("Kids: {0}", totalKids);
+            Console.WriteLine("Adults: {0}", totalAdults);
+            Console.WriteLine("Seniors: {0}", totalSeniors);
+        }
+    }
+}
