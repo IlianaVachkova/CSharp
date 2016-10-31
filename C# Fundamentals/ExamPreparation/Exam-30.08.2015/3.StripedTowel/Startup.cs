fe035785403e247ec1cc1de623488fbf434a6d9c
IlianaVachkova <an_vachkova@yahoo.com>
2016-10-31 03:23:55 +0200

    Added tasks solution for Exam-30.08.2015.

---
+++ C# Fundamentals/ExamPreparation/Exam-30.08.2015/3.StripedTowel/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3.StripedTowel
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n + n / 2; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 3 == 0)
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        Console.Write('.');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
