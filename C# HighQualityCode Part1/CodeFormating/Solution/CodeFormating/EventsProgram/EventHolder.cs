22035e6d0a4f253900f59ebda0e50e14d26e633a
IlianaVachkova <an_vachkova@yahoo.com>
2016-11-02 20:24:39 +0200

    Added homework CodeFormating.

---
+++ C# HighQualityCode Part1/CodeFormating/Solution/CodeFormating/EventsProgram/EventHolder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+public class EventHolder
+{
+    private MultiDictionary<string, Event> byTitle =
+        new MultiDictionary<string, Event>(true);
+    private OrderedBag<Event> byDate =
+        new OrderedBag<Event>();
+
+    public void AddEvent(
+        DateTime date, string title, string location)
+    {
+        Event newEvent = new Event(
+            date, title, location);
+
+        byTitle.Add(title.ToLower(), newEvent);
+        byDate.Add(newEvent);
+
+        Messages.EventAdded();
+    }
+
+    public void DeleteEvents(string titleToDelete)
+    {
+        string title = titleToDelete.ToLower();
+        int removed = 0;
+
+        foreach (var eventToRemove in byTitle[title])
+        {
+            removed++;
+            byDate.Remove(eventToRemove);
+        }
+
+        byTitle.Remove(title);
+        Messages.EventDeleted(removed);
+    }
+
+    public void ListEvents(DateTime date, int count)
+    {
+        OrderedBag<Event>.View eventsToShow =
+            byDate.RangeFrom(
+            new Event(date, string.Empty, string.Empty), true);
+        int shown = 0;
+
+        foreach (var eventToShow in eventsToShow)
+        {
+            if (shown == count)
+            {
+                break;
+            }
+
+            Messages.PrintEvent(eventToShow);
+            shown++;
+        }
+
+        if (shown == 0)
+        {
+            Messages.NoEventsFound();
+        }
+    }
+}
---
+++ C# HighQualityCode Part1/CodeFormating/Solution/CodeFormating/EventsProgram/Messages.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+public static class Messages
+{
+    private static StringBuilder output =
+        new StringBuilder();
+
+    public static StringBuilder Ouput
+    {
+        get
+        {
+            return output;
+        }
+    }
+
+    public static void EventAdded()
+    {
+        output.AppendLine("Event added");
+    }
+
+    public static void EventDeleted(int eventsCount)
+    {
+        if (eventsCount == 0)
+        {
+            NoEventsFound();
+        }
+        else
+        {
+            output.AppendFormat("{0} events deleted", eventsCount);
+            output.AppendLine();
+        }
+    }
+
+    public static void NoEventsFound()
+    {
+        output.AppendLine("No events found");
+    }
+
+    public static void PrintEvent(Event eventToPrint)
+    {
+        if (eventToPrint != null)
+        {
+            output.AppendLine(eventToPrint.ToString());
+        }
+    }
+}
+
